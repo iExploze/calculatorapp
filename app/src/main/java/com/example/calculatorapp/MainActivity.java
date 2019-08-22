f85e4eff70f60dcdccecbb00fd5fdc7f1f8e0ac1
iExploze <54289660+iExploze@users.noreply.github.com>
2019-08-22 17:20:43 -0400

    yeet

--- app/src/main/java/com/example/calculatorapp/MainActivity.java
+++ app/src/main/java/com/example/calculatorapp/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.calculatorapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btn1 = findViewById(R.id.button);
+        final TextView output = findViewById(R.id.textView);
+
+        btn1.setOnClickListener(new View.OnClickListener(){
+           @Override
+           public void onClick(View view){
+               output.setText("1");
+           }
+        });
     }
 }
